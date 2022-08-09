5618d2e8c6cd8691855d333de4809b403b1e710f
tde-nico <101901057+tde-nico@users.noreply.github.com>
2022-08-09 18:16:38 +0200

    11

---
+++ 11_disk/print_hex.asm
@@ -0,0 +1,39 @@
+print_hex:
+	pusha
+	mov cx, 4
+
+	hex_loop:
+		dec cx
+
+		mov ax, dx
+		shr dx, 4
+		and ax, 0xf
+
+		mov bx, HEX_OUT
+		add bx, 2
+		add bx, cx
+
+		cmp ax, 0xa
+		jl set_letter
+
+		add al, 0x27
+		jl set_letter
+	
+	set_letter:
+		
+		add al, 0x30
+		mov byte [bx], al
+
+		cmp cx, 0
+		je end
+		jmp hex_loop
+
+	end:
+		mov bx, HEX_OUT
+		call print
+
+		popa
+		ret
+
+HEX_OUT:
+	db '0x0000', 0
